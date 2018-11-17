55f6db70d58650dc37e37a419b28c0f97b0a6d48
Max O'Cull <max.ocull@protonmail.com>
2018-11-17 14:05:48 -0500

    Update build script filename

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,10 @@ FLAGS="--verbose --cache=/var/cache/live --fslabel=mangOS --title=mangOS-$(date 
 
 if [ "$EDITION" == "libre" ]; then
     echo "Building mangOS Libre edition ..."
-    sudo livecd-creator $FLAGS --config="${HERE}/../mangos-libre.ks" --releasever="libre"
+    sudo livecd-creator $FLAGS --config="${HERE}/../mangOS-libre.ks" --releasever="libre"
 elif [ "$EDITION" == "nonlibre" ]; then
     echo "Building mangOS non-Libre edition ..."
-    sudo livecd-creator $FLAGS --config="${HERE}/../mangos-nonlibre.ks" --releasever="nonlibre"
+    sudo livecd-creator $FLAGS --config="${HERE}/../mangOS-nonlibre.ks" --releasever="nonlibre"
 else
     echo "You must select an edition of mangOS to build."
 fi

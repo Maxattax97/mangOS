a86495d83008e0ae4645e99c6822b7d9103b0326
Max O'Cull <max.ocull@protonmail.com>
2018-11-17 14:31:10 -0500

    Fix release version

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,14 @@
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 EDITION="$1"
-FLAGS="--verbose --cache=/var/cache/live --fslabel=mangOS --title=mangOS-$(date -u "+%y-%m-%dUT%H-%M-%S")"
+FLAGS="--verbose --cache=/var/cache/live --fslabel=mangOS"
 
 if [ "$EDITION" == "libre" ]; then
     echo "Building mangOS Libre edition ..."
-    sudo livecd-creator $FLAGS --config="${HERE}/../mangOS-libre.ks" --releasever="libre"
+    sudo livecd-creator $FLAGS --config="${HERE}/../mangOS-libre.ks" --title=mangOS-libre-$(date -u "+%y-%m-%dUT%H-%M-%S")
 elif [ "$EDITION" == "nonlibre" ]; then
     echo "Building mangOS non-Libre edition ..."
-    sudo livecd-creator $FLAGS --config="${HERE}/../mangOS-nonlibre.ks" --releasever="nonlibre"
+    sudo livecd-creator $FLAGS --config="${HERE}/../mangOS-nonlibre.ks" --title=mangOS-nonlibre-$(date -u "+%y-%m-%dUT%H-%M-%S")
 else
     echo "You must select an edition of mangOS to build."
 fi
